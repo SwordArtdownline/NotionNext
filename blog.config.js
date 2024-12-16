c0fd676c11baf5fc512b60b646eb8a9be27ecf52
NiuNiu <140424449+SwordArtdownline@users.noreply.github.com>
2024-12-16 10:48:06 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -36,7 +36,7 @@ const BLOG = {
   CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || 'https://github.com/SwordArtdownline', // 你的github个人主页 例如 https://github.com/tangly1024
   CONTACT_TELEGRAM: process.env.NEXT_PUBLIC_CONTACT_TELEGRAM || 'https://i.111666.best/image/A34QlkfMr2POXTHuGozmM7.jpeg', // 你的telegram 地址 例如 https://t.me/tangly_1024
   CONTACT_LINKEDIN: process.env.NEXT_PUBLIC_CONTACT_LINKEDIN || '', // 你的linkedIn 首页
-  CONTACT_INSTAGRAM: process.env.NEXT_PUBLIC_CONTACT_INSTAGRAM || 'https://i.111666.best/image/A34QlkfMr2POXTHuGozmM7.jpeg', // 您的instagram地址
+  CONTACT_INSTAGRAM: process.env.NEXT_PUBLIC_CONTACT_INSTAGRAM || '', // 您的instagram地址
   CONTACT_BILIBILI: process.env.NEXT_PUBLIC_CONTACT_BILIBILI || 'https://space.bilibili.com/629107630?spm_id_from=333.1007.0.0', // B站主页
   CONTACT_YOUTUBE: process.env.NEXT_PUBLIC_CONTACT_YOUTUBE || 'https://www.youtube.com/@N.N-XXD', // Youtube主页
   CONTACT_XIAOHONGSHU: process.env.NEXT_PUBLIC_CONTACT_XIAOHONGSHU || 'https://i.111666.best/image/A34QlkfMr2POXTHuGozmM7.jpeg', // 小红书主页
